6b0dc561c084255cbb9b5dd97b3a3487da4f26d6
alearends <alejandroarends77@gmail.com>
2024-03-20 23:25:08 +0100

    subiendo template

---
+++ README.md
@@ -0,0 +1,15 @@
+## Backend
+
+Cors, Express, Mongoose, Nodemon 
+
+
+
+## Frontend
+
+React, React-Animation, React-Form, React-Prop-Types, React-Router-Dom, 
+Tailwindcss, Tailwindcss-Animate
+Typescript
+Vite
+Zod
+
+
---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className="text-3xl font-bold underline">Template</h1>
+      <h2 className="text-2xl font-bold underline">Frontend: React Vite Typescript Tailwind</h2>
+      
+    </>
+  )
+}
+
+export default App
